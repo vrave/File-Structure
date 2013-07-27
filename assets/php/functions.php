ea7a8b3f057f9b90308de0e3093fdf7956fed4dc
vrave <foodtripbadtrip@gmail.com>
2013-07-27 19:58:45 -0400

    Sample Project File Structure
    with bootstrap and bootstrap-editable

---
+++ assets/php/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// If you include this file, no need to include global.php
+require_once 'global.php';
+
+// Functions goes here
+
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'assets/php/global.php';
+
+$pagetitle = "Index";
+include 'assets/php/includes/htmlheader.php';
+?>
+<!-- BODY GOES HERE -->
+<!-- NO NEED TO PUT THE <BODY> TAG -->
+
+<!-- .wrap starts here -->
+<div class="wrap">
+  
+</div> <!-- .wrap ends here -->
+
+<?php 
+include 'assets/php/includes/htmlfooter.php';
+ ?>
