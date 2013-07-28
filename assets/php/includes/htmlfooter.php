5b8e02cd131be79355bfa8987792869f65c0e832
vrave <foodtripbadtrip@gmail.com>
2013-07-27 20:10:44 -0400

    Revert "TEST"
    This reverts commit cf6fd25ecb76c9300d2a18cf59b517c4bb94b40e.

---
+++ assets/php/includes/htmlfooter.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/../global.php';
+?>
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+    
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<!-- Bootstrap JS -->
+<script src="<?=ROOT_DIR?>/assets/bootstrap/js/bootsrap.min.js"></script>
+</body>
+</html>
